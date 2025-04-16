dcc9473b735f3261e151879cc9b2551aa386d1ea
Nurmuhammad14456 <nabdujalilov036@gmail.com>
2025-04-16 15:43:04 +0500

    loyha

---
+++ app.js
@@ -0,0 +1,44 @@
+{
+  // birinchi masala
+  // let a = parseFloat(prompt("Sonni kiriting:"));
+  // let javob = Math.round(a);
+  // console.log("Yaxlitlangan qiymat", javob);
+}
+
+{
+  // ikkinchii masala
+  // let a = parseFloat(prompt("Xoxlagan son kiriting"));
+  // let model = Math.abs(a);
+  // console.log("Musbat qiymat", model);
+}
+
+{
+  // uchinchi masala
+  //   let baholar = [];
+  //   for (let i = 0; i < 5; i++) {
+  //     let randomBaho = Math.floor(Math.random() * 101); // 0-100 oralig‘ida
+  //     baholar.push(randomBaho);
+  //   }
+  //   let sum = baholar.reduce((a, b) => a + b, 0);
+  //   let average = sum / baholar.length;
+  //   let rounded = Math.round(average);
+  //   console.log("Baholar:", baholar);
+  //   console.log("O‘rtacha ball:", rounded);
+}
+
+{
+  // 4 masala
+  // let dollar = 100;
+  // let kurs = Math.random() * (13 - 10) + 10;
+  // let som = dollar * kurs;
+  // console.log("Kurs:", kurs.toFixed(4));
+  // console.log("Somda:", som.toFixed(2));
+}
+{
+   // 5 chi masala 
+//   let year = Math.floor(Math.random() * (2026 - 2000)) + 2000;
+//   let month = Math.floor(Math.random() * 12) + 1;
+//   let day = Math.floor(Math.random() * 28) + 1;
+
+//   console.log(`Tasodifiy sana: ${year}-${month}-${day}`);
+}
